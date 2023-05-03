4b187d2836ac1431fe569a5da761e31920508ab1
sohel <mr.sohelsyed@gmail.com>
2023-05-03 20:01:29 +0530

    added basic structure for doctor login

--- project/src/components/Doctor's/DoctorLogin.jsx
+++ project/src/components/Doctor's/DoctorLogin.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import { Box, FormLabel, Input, FormControl, Button } from "@chakra-ui/react";
 export const DoctorLogin = () => {
   return (
-    <div>DoctorLogin</div>
-  )
-}
+    <Box>
+      <Box w="30%">
+        <FormControl>
+          <FormLabel color="#61876e">Email</FormLabel>
+          <Input type="email" borderColor="#61876e" />
+          <FormLabel color="#61876e">Password</FormLabel>
+          <Input type="Password" borderColor="#61876e" />
+          <FormLabel></FormLabel>
+          <Button bgColor={"#61876e"} color="white" w="100%">
+            Login
+          </Button>
+        </FormControl>
+      </Box>
+    </Box>
+  );
+};
