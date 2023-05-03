7a1a5d4dbcf86d4b8d0ba4a9b3c371aa3de71b43
Sakshi Mulekar <sakshimulekar2017@gmail.com>
2023-05-03 16:41:14 +0530

    fjf

--- project/src/redux/store.js
+++ project/src/redux/store.js
@@ -1,12 +1,18 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
-
-const rootReducer = {
-    
-}
+import {reducer as authReducer} from "./AuthReducer/reducer";
+import {reducer as bookReducer} from "./BookingReducer/reducer";
+import {reducer as doctorReducer} from "./DoctorReducer/reducer";
+import {reducer as patientReducer} from "./PatientReducer/reducer";
+const rootReducer = combineReducers({
+    authReducer,
+    bookReducer,
+    doctorReducer,
+    patientReducer,
+})
 
 
    
 
 
-export const store = legacy_createStore(combineReducers(rootReducer),applyMiddleware(thunk))
+export const store = legacy_createStore(rootReducer,applyMiddleware(thunk))
