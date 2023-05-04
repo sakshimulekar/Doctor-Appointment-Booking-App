4bf1cf0380647cdbfd7c1c09aec2357ca8b2e857
sohel <mr.sohelsyed@gmail.com>
2023-05-04 12:48:05 +0530

    reducer changes

--- project/src/redux/store.js
+++ project/src/redux/store.js
@@ -1,34 +1,17 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
-<<<<<<< HEAD
-import { reducer as BookingReducer } from "./BookingReducer";
-import { reducer as DoctorReducer } from "./doctorReducer/reducer";
-const rootReducer = {
-  BookingReducer,
-  DoctorReducer,
-};
 
-export const store = legacy_createStore(
-  combineReducers(rootReducer),
-  applyMiddleware(thunk)
-);
-=======
-
-import {reducer as authReducer} from "./AuthReducer/reducer";
-import {reducer as bookReducer} from "./BookingReducer/reducer";
-import {reducer as doctorReducer} from "./DoctorReducer/reducer";
-import {reducer as patientReducer} from "./PatientReducer/reducer";
+import { reducer as authReducer } from "./AuthReducer/reducer";
+import { reducer as bookReducer } from "./BookingReducer/reducer";
+import { reducer as doctorReducer } from "./DoctorReducer/reducer";
+import { reducer as patientReducer } from "./PatientReducer/reducer";
 
 const rootReducer = combineReducers({
-    authReducer,
-    bookReducer,
-    doctorReducer,
-    patientReducer,
-})
+  authReducer,
+  bookReducer,
+  doctorReducer,
+  patientReducer,
+});
 
-
-   
-
-export const store = legacy_createStore(rootReducer,applyMiddleware(thunk))
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 //export const store = legacy_createStore(patientReducer,applyMiddleware(thunk))
->>>>>>> 84b313f1bdcc4d977f099eb21623bdb8b9bb9cdc
