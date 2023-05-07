d50bb038bb9ae21253afb6167619699fea7011ee
sohel <mr.sohelsyed@gmail.com>
2023-05-07 12:11:28 +0530

    fix the bug in action js

--- project/src/redux/BookingReducer/action.js
+++ project/src/redux/BookingReducer/action.js
@@ -1,16 +1,20 @@
-import { DOCTOR_FAILURE, DOCTOR_REQUEST, GET_DOCTOR_SUCCESS } from "./actionType"
-import axios from "axios"
+import {
+  DOCTOR_FAILURE,
+  DOCTOR_REQUEST,
+  GET_DOCTOR_SUCCESS,
+} from "./actionType";
+import axios from "axios";
 
-export const getDoctor=(paramObj)=>(dispatch)=>{
-    dispatch({type:DOCTOR_REQUEST})
-    axios.get('http://localhost:8080/doctors',paramObj)
-    .then((response)=>{
-
-     //console.log(response.data)
-     dispatch({type:GET_DOCTOR_SUCCESS,payload:response.data})   
+export const getDoctor = (paramObj) => (dispatch) => {
+  dispatch({ type: DOCTOR_REQUEST });
+  axios
+    .get("http://localhost:8080/doctors", paramObj)
+    .then((response) => {
+      //console.log(response.data)
+      dispatch({ type: GET_DOCTOR_SUCCESS, payload: response.data });
     })
-    .catch((error)=>{
-        console.log(error)
-        dispatch({type:DOCTOR_FAILURE})
-    })
-
+    .catch((error) => {
+      console.log(error);
+      dispatch({ type: DOCTOR_FAILURE });
+    });
+};
