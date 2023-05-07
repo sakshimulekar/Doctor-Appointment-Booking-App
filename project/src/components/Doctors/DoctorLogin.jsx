623f003cf972c968226a9abe12a51cabda55e92b
sohel <mr.sohelsyed@gmail.com>
2023-05-07 21:06:19 +0530

    fixing login  of doctor page

--- project/src/components/Doctors/DoctorLogin.jsx
+++ project/src/components/Doctors/DoctorLogin.jsx
@@ -1,23 +1,35 @@
-import React from "react";
-import {
-  Box,
-  FormLabel,
-  Input,
-  FormControl,
-  Button,
-  Text,
-} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, FormLabel, Input, FormControl, Button } from "@chakra-ui/react";
 export const DoctorLogin = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = () => {
+    console.log(username, password);
+    setPassword("");
+    setUsername("");
+  };
   return (
     <Box bg={"one"}>
       <Box w="30%" m={"auto"}>
         <FormControl>
           <FormLabel color="two">Email</FormLabel>
-          <Input type="email" borderColor="two" />
+          <Input
+            type="email"
+            borderColor="two"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </FormControl>
+        <FormControl>
           <FormLabel color="two">Password</FormLabel>
-          <Input type="Password" borderColor="two" />
+          <Input
+            type="Password"
+            borderColor="two"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <FormLabel></FormLabel>
-          <Button bgColor="two" color="white" w="100%">
+          <Button bgColor="two" color="white" w="100%" onClick={handleSubmit}>
             Login
           </Button>
         </FormControl>
