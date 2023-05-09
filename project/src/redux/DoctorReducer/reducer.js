934dc63d4ec54dc1e14ee8f1de92ee6d835fa41a
sohel <mr.sohelsyed@gmail.com>
2023-05-09 14:15:16 +0530

    added files

--- project/src/redux/DoctorReducer/reducer.js
+++ project/src/redux/DoctorReducer/reducer.js
@@ -3,6 +3,7 @@ import {
   GET_DOCTORS_PENDING_SH,
   GET_DOCTORS_SUCCESS_SH,
   POST_DOCTOR_SUCCESS_SH,
+  UPDATE_STATUS,
 } from "./actionTypes";
 
 const initalState = {
@@ -21,6 +22,7 @@ export const reducer = (state = initalState, { type, payload }) => {
       return { ...state, isLoading: false, isError: true };
     case POST_DOCTOR_SUCCESS_SH:
       return { ...state, isLoading: false, LogedInDoctor: payload };
+   
     default:
       return state;
   }
