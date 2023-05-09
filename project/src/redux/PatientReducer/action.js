9093ba86d0b932d30906fb3369d3f2d86e276bf2
sohel <mr.sohelsyed@gmail.com>
2023-05-09 15:52:18 +0530

    added

--- project/src/redux/PatientReducer/action.js
+++ project/src/redux/PatientReducer/action.js
@@ -40,24 +40,9 @@ export const getPatient = () => (dispatch) => {
 export const updateStatusFn = (id, status) => (dispatch) => {
   dispatch({ type: GET_PATIENT_REQ });
 
-  let data;
- 
-  // axios
-  //   .get(`http://localhost:8080/patients/${id}`)
-  //   .then((res) => {
-  //     console.log("single-data", res.data);
-  //     data = res.data.status;
-
-     
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
-
   return axios
     .patch(`http://localhost:8080/patients/${id}`, { status: !status })
     .then((res) => {
-      
       dispatch({ type: UPDATE_STATUS, payload: res.data });
     });
 };
