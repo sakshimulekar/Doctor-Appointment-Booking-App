749967120d9d5c84114b12aedfe3d78e60f9ac9e
sohel <mr.sohelsyed@gmail.com>
2023-05-09 15:54:30 +0530

    added

--- project/src/redux/doctorReducer/action.js
+++ project/src/redux/doctorReducer/action.js
@@ -3,8 +3,8 @@ import {
   GET_DOCTORS_PENDING_SH,
   GET_DOCTORS_SUCCESS_SH,
   POST_DOCTOR_SUCCESS_SH,
-  UPDATE_STATUS,
 } from "./actionTypes";
+
 import axios from "axios";
 
 export const getDoctorsFn = () => (dispatch) => {
@@ -46,5 +46,3 @@ export const getLoginDoctorFn = () => (dispatch) => {
       console.log(err);
     });
 };
-
-
